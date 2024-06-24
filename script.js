5025a77d40ff44cbb727a9a2858087b47fe58c93
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-06-23 23:59:59 -0400

    Clear All Tasks && Remove task

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 const taskInput = document.getElementById("task_input");
+const removeAllTasks = document.getElementById("remove_all_task");
 
 let task_list = [];
-function renderTask() {
+function renderTask(event) {
     const userInput = document.getElementById("task_input").value;
     const unorderedList = document.getElementById("unordered_list");
+    const icon = document.createElement("i");
     const list = document.createElement("li");
+
     // basically e.preventDefault(); lol
-    if(userInput.length <= 0) {
+    if (userInput.length <= 0) {
         return;
     }
 
     task_list.push(userInput);
     task_list.forEach(task_item => {
         list.textContent = task_item;
-        unorderedList.appendChild(list);    
+        icon.classList.add("bi", "bi-x", "float-end");
+        icon.style.cursor = "pointer";
+        unorderedList.appendChild(list);
+        list.appendChild(icon);
+
+    });
+    icon.addEventListener("click", function () {
+        this.remove();
+        list.remove();
+
+    });
+
+    removeAllTasks.addEventListener("click", function () {
+        unorderedList.textContent = "";
     });
 }
 
 /**
- * calls renderTask() if key is equal to enter!
+ * calls renderTask() if key is equal to enter
  */
-taskInput.addEventListener("keyup", function(event) {
-    if(event.key == "Enter") {
+taskInput.addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
         renderTask();
     }
 });
